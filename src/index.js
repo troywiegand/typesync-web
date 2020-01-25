865a07f4abe7fcecf6152ed2cc57d6af2167c20b
David Purdum <purdum41@gmail.com>
2020-01-25 15:05:37 -0500

    Add BOOT STRAP

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App.jsx';
 import './index.css';
 
+<style src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"></style>
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
